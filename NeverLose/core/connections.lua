5f22ea4917274e4b85e564736ef96176797ef676
idk <neyqwe6@gmail.com>
2026-06-28 23:12:01 +0500

    Update connections.lua

--- NeverLose/core/connections.lua
+++ NeverLose/core/connections.lua
@@ -3,17 +3,17 @@
 local Connections = {}
 
 function Connections.Add(Storage, connection)
-    table.insert(Storage.connections, connection)
-    return connection
+	table.insert(Storage.connections, connection)
+	return connection
 end
 
 function Connections.DisconnectAll(Storage)
-    for _, c in pairs(Storage.connections) do
-        if c and c.Connected then
-            c:Disconnect()
-        end
-    end
-    Storage.connections = {}
+	for _, c in pairs(Storage.connections) do
+		if c and c.Connected then
+			c:Disconnect()
+		end
+	end
+	Storage.connections = {}
 end
 
 return Connections
