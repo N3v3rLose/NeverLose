1c6c3288ec2aa716c091f02918d386541297bcec
idk <neyqwe6@gmail.com>
2026-06-28 23:10:06 +0500

    Refactor UI animations code formatting

--- NeverLose/ui/animations.lua
+++ NeverLose/ui/animations.lua
@@ -2,13 +2,12 @@
 
 local Animations = {}
 
--- Placeholder для будущих анимаций открытия/закрытия
 function Animations.ShowMenu(MainFrame)
-    MainFrame.Visible = true
+	MainFrame.Visible = true
 end
 
 function Animations.HideMenu(MainFrame)
-    MainFrame.Visible = false
+	MainFrame.Visible = false
 end
 
 return Animations
